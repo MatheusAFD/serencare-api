902efea147e4f8ec234f4fdd821810ba80b8f533
Matheus <matheus.daf@outlook.com>
2024-01-31 23:10:59 -0300

    :sparkles: Add default crud functions generated by nest command

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
   async create(createUserDto: CreateUserDto) {
     const { email, name } = createUserDto;
 
@@ -9,8 +19,23 @@
     });
 
     return newUser;
+  }
+
   async findAll() {
     const users = await this.prisma.user.findMany();
 
     return users;
   }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
