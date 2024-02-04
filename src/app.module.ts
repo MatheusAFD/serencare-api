17afdd461af177c1ef8ba14a747703aae91dc140
Matheus <matheus.daf@outlook.com>
2024-02-03 22:54:54 -0300

    :rotating_light: Fix linter warn

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { AuthModule } from './modules/auth/auth.module'
+import { UserModule } from './modules/user/user.module'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
