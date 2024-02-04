452905501520e46f3e2270fa1d6a78269f853eea
Matheus <matheus.daf@outlook.com>
2024-02-03 22:55:14 -0300

    :sparkles: Create custom decorator to use on api routes

---
+++ src/decorators/user/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+
+import { User } from 'src/modules/user/entities/user.entity'
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest()
+    return request.user as User
+  }
+)
