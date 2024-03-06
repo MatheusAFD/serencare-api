dbad3c5b05c3fa42ebe963168e9d6fcddc12d5a3
Matheus <matheus.daf@outlook.com>
2024-03-05 21:25:48 -0300

    :sparkles: Add get-me use as provider on user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,11 +4,12 @@ import { JwtService } from '@nestjs/jwt'
 
 import { PrismaService } from 'prisma/prisma.service'
 
+import { GetMeUseCase } from './use-cases/get-me-usecase'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService, JwtService, Reflector]
+  providers: [UserService, PrismaService, JwtService, Reflector, GetMeUseCase]
 })
 export class UserModule {}
