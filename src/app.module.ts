82e015e56fc4047022010963d2abba0b04cf4dea
Matheus <matheus.daf@outlook.com>
2024-03-10 14:19:22 -0300

    :sparkles: Add active plan module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,30 @@ import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD, Reflector } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 
+import { ActiveCompanyPlanModule } from './modules/active-company-plan/active-company-plan.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { JwtAuthGuard } from './modules/auth/jwt-auth.guard'
 import { RolesGuard } from './modules/auth/role.guard'
+import { CompanyModule } from './modules/company/company.module'
+import { PlanModule } from './modules/plan/plan.module'
+import { RoomModule } from './modules/room/room.module'
+import { UnitModule } from './modules/unit/unit.module'
 import { UserModule } from './modules/user/user.module'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UnitModule } from './modules/unit/unit.module';
-import { RoomModule } from './modules/room/room.module';
-import { CompanyModule } from './modules/company/company.module';
-import { PlanModule } from './modules/plan/plan.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, UnitModule, RoomModule, CompanyModule, PlanModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    AuthModule,
+    UnitModule,
+    RoomModule,
+    CompanyModule,
+    PlanModule,
+    ActiveCompanyPlanModule
+  ],
   controllers: [AppController],
   providers: [
     AppService,
