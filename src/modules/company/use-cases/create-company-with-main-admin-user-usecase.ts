38872999be5898ae4041e738e46f1ca7cbde757d
matheusafd <matheus.daf@outlook.com>
2024-08-19 20:29:15 -0300

    refactor: change activeCompanyPlan to optional

--- src/modules/company/use-cases/create-company-with-main-admin-user-usecase.ts
+++ src/modules/company/use-cases/create-company-with-main-admin-user-usecase.ts
@@ -6,28 +6,11 @@ import { encryptData } from '@common/utils/encrypt-data'
 
 import { CreateCompanyAndUserDTO } from '../dto/create-company-and-user.dto'
 
-import { addDays } from 'date-fns'
-
 @Injectable()
 export class CreateCompanyWithMainAdminUserUseCase {
   constructor(private readonly prisma: PrismaService) {}
 
   async execute(createCompanyAndUserDTO: CreateCompanyAndUserDTO) {
-    const adminRole = await this.prisma.role.findUniqueOrThrow({
-      where: {
-        type: 'ADMIN'
-      },
-      select: {
-        id: true
-      }
-    })
-
-    const plan = await this.prisma.plan.findUniqueOrThrow({
-      where: {
-        id: createCompanyAndUserDTO.planId
-      }
-    })
-
     const hashedPassword = await encryptData(createCompanyAndUserDTO.password)
 
     const companyWithAdminUser = await this.prisma.company.create({
@@ -35,23 +18,14 @@ export class CreateCompanyWithMainAdminUserUseCase {
         name: createCompanyAndUserDTO.name,
         isActive: false,
         cnpj: createCompanyAndUserDTO.cnpj,
-        activeCompanyPlan: {
-          create: {
-            planId: createCompanyAndUserDTO.planId,
-            isActive: false,
-            remainingDaysWithActivePlan: plan.duration,
-            startDate: new Date(),
-            endDate: addDays(new Date(), plan.duration)
-          }
-        },
         users: {
           create: {
             email: createCompanyAndUserDTO.email,
             name: createCompanyAndUserDTO.userName,
             password: hashedPassword,
-            role: {
+            roles: {
               connect: {
-                id: adminRole.id
+                type: 'SUPER_ADMIN'
               }
             }
           }
