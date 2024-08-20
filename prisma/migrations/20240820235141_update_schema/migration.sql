4706a11420f11ebb16b313105be79718c3098a0e
matheusafd <matheus.daf@outlook.com>
2024-08-20 20:51:54 -0300

    feat: add prisma migration

---
+++ prisma/migrations/20240820235141_update_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `activeCompanyPlanId` on the `Company` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Company" DROP COLUMN "activeCompanyPlanId";
